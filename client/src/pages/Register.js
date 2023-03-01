167e6270db4c511f0862ef629687e7c6ba2e3271
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-02-28 23:35:43 -0500

    8. Context - set up cont

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import { Logo, FormRow, Alert } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
-import 
+import { useAppContext } from '../context/appContext'
+
 // wrapper is for css 
 
 const initialState = {
